e2d18f1fecdedfceb7c7dac8ee96af3710401f39
Max Halford <maxhalford25@gmail.com>
2020-07-16 01:22:51 +0200

    0.15020865743141465

--- median_filter.pyx
+++ median_filter.pyx
@@ -9,7 +9,7 @@ ctypedef np.uint8_t uint8
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef (uint8, uint) find_median(uint [:] hist):
+cdef (uint8, uint) find_quantile(uint [:] hist, float q):
 
     cdef uint cumsum
     cdef uint total
@@ -17,19 +17,20 @@ cdef (uint8, uint) find_median(uint [:] hist):
     for i in range(hist.shape[0]):
         total += hist[i]
     cdef uint8 median
+    cdef uint threshold = int(total * q)
 
     for median in range(hist.shape[0]):
         cumsum += hist[median]
-        if cumsum >= total // 2:
+        if cumsum >= threshold:
             return median, cumsum
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef median_filter(uint8[:, :] img, int width):
+cpdef quantile_filter(uint8[:, :] img, int width, float q):
 
-    cdef uint8 [:, :] med = np.zeros_like(img)
-    cdef uint8 median
+    cdef uint8 [:, :] quantiles = np.zeros_like(img)
+    cdef uint8 quantile
     cdef uint cumsum
 
     cdef uint [:] hist = np.zeros(256, dtype=np.uint)
@@ -41,6 +42,7 @@ cpdef median_filter(uint8[:, :] img, int width):
     cdef int removed_col
     cdef int added_col
     cdef uint8 val
+    cdef uint threshold
 
     cdef int n_rows
     cdef int n_cols
@@ -55,8 +57,8 @@ cpdef median_filter(uint8[:, :] img, int width):
             for c in range(width + 1):
                 hist[img[r, c]] += 1
 
-        median, cumsum = find_median(hist)
-        med[row, 0] = median
+        quantile, cumsum = find_quantile(hist, q)
+        quantiles[row, 0] = quantile
 
         for col in range(1, img.shape[1]):
 
@@ -66,7 +68,7 @@ cpdef median_filter(uint8[:, :] img, int width):
                 for r in range(max(0, row - width), min(row + width + 1, img.shape[0])):
                     val = img[r, removed_col]
                     hist[val] -= 1
-                    if val <= median:
+                    if val <= quantile:
                         cumsum -= 1
 
             # Update the histogram with the values from the newest column
@@ -75,22 +77,24 @@ cpdef median_filter(uint8[:, :] img, int width):
                 for r in range(max(0, row - width), min(row + width + 1, img.shape[0])):
                     val = img[r, added_col]
                     hist[val] += 1
-                    if val <= median:
+                    if val <= quantile:
                         cumsum += 1
 
             # Determine how many pixels make up the current region
             n_rows = min(row + width, img.shape[0] - 1) - max(0, row - width) + 1
             n_cols = min(col + width, img.shape[1] - 1) - max(0, col - width) + 1
             size = n_rows * n_cols
+            threshold = int(size * q)
 
-            # Update the median
-            while cumsum > size // 2 or hist[median] == 0:
-                cumsum -= hist[median]
-                median -= 1
-            while cumsum < size // 2 or hist[median] == 0:
-                median += 1
-                cumsum += hist[median]
+            # Update the quantile
 
-            med[row, col] = median
+            while cumsum > threshold or hist[quantile] == 0:
+                cumsum -= hist[quantile]
+                quantile -= 1
+            while cumsum < threshold or hist[quantile] == 0:
+                quantile += 1
+                cumsum += hist[quantile]
 
-    return np.asarray(med)
+            quantiles[row, col] = quantile
+
+    return np.asarray(quantiles)
